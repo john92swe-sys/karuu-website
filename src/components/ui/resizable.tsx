4478a7d304cd09f683dd2d4cb33d7953c6fffb66
john92swe-sys <john92swe@gmail.com>
2026-07-31 10:14:36 +0800

    Fix resizable panels API

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -2,29 +2,28 @@
 
 import { GripVerticalIcon } from 'lucide-react';
 import {
-  Group,
+  PanelGroup,
   Panel,
-  Separator,
-  type GroupProps,
+  PanelResizeHandle,
+  type PanelGroupProps,
   type PanelProps,
-  type SeparatorProps,
+  type PanelResizeHandleProps,
 } from 'react-resizable-panels';
 
 import { cn } from '@/lib/utils';
 
 function ResizablePanelGroup({
   className,
-  orientation = 'horizontal',
+  direction = 'horizontal',
   ...props
-}: GroupProps) {
+}: PanelGroupProps) {
   return (
-    <Group
+    <PanelGroup
       data-slot="resizable-panel-group"
-      data-panel-group-direction={orientation}
-      orientation={orientation}
+      direction={direction}
       className={cn(
         'flex h-full w-full data-[panel-group-direction=vertical]:flex-col',
-        className,
+        className
       )}
       {...props}
     />
@@ -39,15 +38,15 @@ function ResizableHandle({
   withHandle,
   className,
   ...props
-}: SeparatorProps & {
+}: PanelResizeHandleProps & {
   withHandle?: boolean;
 }) {
   return (
-    <Separator
+    <PanelResizeHandle
       data-slot="resizable-handle"
       className={cn(
         'bg-border focus-visible:ring-ring relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-x-0 data-[panel-group-direction=vertical]:after:-translate-y-1/2 [&[data-panel-group-direction=vertical]>div]:rotate-90',
-        className,
+        className
       )}
       {...props}
     >
@@ -56,7 +55,7 @@ function ResizableHandle({
           <GripVerticalIcon className="size-2.5" />
         </div>
       )}
-    </Separator>
+    </PanelResizeHandle>
   );
 }
 
